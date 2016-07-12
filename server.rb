5c987db14f236dbd978d595c8415be45b0d4488c
MiguelDotL <miguel.d.lozano@gmail.com>
2016-07-12 18:03:42 -0400

    initial commit - My first web server

--- server.rb
+++ server.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
 get '/' do
-"hello world"
+# "hello world"
+  redirect '/hello.txt'
+  # File.new('/public/hello.txt').readlines # <-- Or This <--
 end
 
 get '/sinatra' do
-"heya frank"
+"heya franky"
 end
